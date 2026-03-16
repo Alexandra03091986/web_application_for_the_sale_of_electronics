f6960ab293fc13475562b211c7a51333a752238f
alexandra andr <shura_1986@mail.ru>
2026-03-16 23:36:21 +0300

    add model Contact

--- network/models.py
+++ network/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Модель контактов"""
+    email = models.EmailField(verbose_name="Email")
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    street = models.CharField(max_length=100, verbose_name="Улица")
+    house_number = models.CharField(max_length=20, verbose_name="Номер дома")
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+
+    def __str__(self):
+        return f"{self.country}, {self.city}, {self.street} {self.house_number}"
