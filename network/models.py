6e25ed892c42feeed15c8718ea69b823d5f9590e
alexandra andr <shura_1986@mail.ru>
2026-03-17 00:00:07 +0300

    add models Product and Supplier

--- network/models.py
+++ network/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -15,3 +16,58 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.country}, {self.city}, {self.street} {self.house_number}"
+
+
+class Product(models.Model):
+    """Модель продукта"""
+    name = models.CharField(max_length=255, verbose_name="Название")
+    model = models.CharField(max_length=255, verbose_name="Модель")
+    release_date = models.DateField(verbose_name="Дата выхода")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return f"{self.name} {self.model}"
+
+
+class Supplier(models.Model):
+    """Модель поставщика"""
+    LEVEL_CHOICES = [
+        (0, "Завод"),
+        (1, "Розничная сеть"),
+        (2, "Индивидуальный предприниматель"),
+    ]
+    name = models.CharField(max_length=255, verbose_name="Название")
+    level = models.IntegerField(choices=LEVEL_CHOICES, verbose_name="Уровень сети")
+    contact = models.OneToOneField(
+        Contact,
+        on_delete=models.CASCADE,
+        verbose_name="Контакт",
+        related_name="supplier"
+    )
+    products = models.ManyToManyField(Product, verbose_name="Продукты")
+    supplier = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Поставщик",
+        related_name="clients"
+    )
+    debt = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name="Задолженность перед поставщиком"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    class Meta:
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+
+    def __str__(self):
+        return self.name
